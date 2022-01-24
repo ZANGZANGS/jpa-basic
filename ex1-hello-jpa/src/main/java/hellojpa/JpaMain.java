4d0d9442d3b1572506acb2cbec9e9c66d2229459
zangzangs <kimjangh94@gmail.com>
2022-01-24 14:23:08 +0900

    즉시 로딩과 지연 로딩

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,47 +19,33 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
+            Team teamB = new Team();
+            teamB.setName("teamA");
+            em.persist(teamB);
+            
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("member2");
+            member2.setTeam(teamB);
             em.persist(member2);
 
 
-            em.flush();
-            em.clear();
-
-            Member findMember1 = em.getReference(Member.class, member1.getId());
-            System.out.println("ref = " + findMember1.getClass());
-
-            Member findMember2 = em.find(Member.class, member1.getId());
-            System.out.println("find = " + findMember2.getClass());
-
-            System.out.println("ref == find : " + (findMember1 == findMember2));
 
             em.flush();
             em.clear();
 
-            //=============================//
+            List<Member> members = em.createQuery("select m from Member m ", Member.class).getResultList();
 
-            Member member = new Member();
-            member.setUsername("zzs");
-            em.persist(member);
 
-            em.flush();
-            em.clear();
-
-            Member refMember = em.getReference(Member.class, member.getId());
-            System.out.println("refMember = " + refMember.getClass());
-            
-//            refMember.getUsername(); //초기화를 위해 이런 메서드를 호출하면 없어 보인다
-            Hibernate.initialize(refMember); //강제 초기화
-            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember)); //프록시 로딩 체크
-
-            
+            members.get(0).getTeam().getName();
             
             
             tx.commit();
